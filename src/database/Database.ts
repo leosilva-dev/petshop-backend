f123c77a48749f5eff6c437a65c00d7702db472b
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-03-12 00:05:59 -0300

    Modificando exemplo de .env e simplificando o start do server

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -2,10 +2,10 @@ require('dotenv/config');
 import  mongoose from "mongoose";
 
 function connectToDatabase(){
-mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@clustermongo.kgrer.mongodb.net/mylinks?retryWrites=true&w=majority`, {autoIndex:true, autoCreate:true});
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clustermongo.kgrer.mongodb.net/mylinks?retryWrites=true&w=majority`, {autoIndex:true, autoCreate:true});
   const db = mongoose.connection;
 
-  db.on("error", (error) => console.error('error'));
+  db.on("error", (error) => console.error('error '+error));
   db.once("open", () => console.log(' 🎲 database is running'));
 }
 
--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,13 @@ import express from 'express';
 import { routes } from "./routes/Routes";
 import {connectToDatabase} from './database/Database'
 
+connectToDatabase();
 
-const startServer = () => {
-    const app = express();
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.listen(process.env.PORT || 3333);
+console.log('\n 🔥 server is runnning on port '+process.env.PORT)  
+
     
-    app.use(express.json());
-    app.use(routes);
-    connectToDatabase();
-    app.listen(process.env.PORT || 3333);
 
-    console.log('\n 🔥 server is runnning on port '+process.env.PORT)  
-}
-
-if (process.env.NODE_ENV === 'production') {
-    startServer();
-  } else {
-    startServer();
-  }
