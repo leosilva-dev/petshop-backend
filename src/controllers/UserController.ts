01d476f34acf9a5fac20a90b120b7a428141c6fc
Leonardo da Silva <silvaleonardo.ti@gmail.com>
2022-03-12 00:06:42 -0300

    Incluindo metodos de CRUD de user (getById, Update, Delete)

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,7 +2,17 @@ import {Request, Response} from 'express'
 import {User} from '../models/User'
 
 
-const GetAll = async (_:Request, res: Response) => {
+const getById = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        const userById = await User.findById(id)
+        res.status(200).json(userById)        
+    } catch (error) {
+        res.status(500).json({error:error})
+    }
+}
+
+const getAll = async (_:Request, res: Response) => {
     try {
         const allUsers = await User.find()
         res.status(200).json(allUsers)
@@ -11,9 +21,10 @@ const GetAll = async (_:Request, res: Response) => {
     }
 }
 
-const Create = async (req: Request, res: Response) => {
+const create = async (req: Request, res: Response) => {
     try {
-        const newUser = await new User(req.body)
+        const {name, email, password} = req.body
+        const newUser = await new User({name, email, password})
         newUser.save()
         res.status(201).json(newUser)
     } catch (error) {
@@ -21,7 +32,31 @@ const Create = async (req: Request, res: Response) => {
     }
 }
 
+const updateById = async (req:Request, res:Response) => {
+    try {
+        const id = req.params.id
+        const {name, email, password} = req.body
+        const userUpdated = await User.findByIdAndUpdate(id, {name, email, password})
+        res.status(200).json({userUpdated})
+    } catch (error) {
+        res.status(500).json({error:error})
+    }
+}
+
+const deleteById = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        const resultOfAction = await User.deleteOne({_id: id})
+        res.status(200).json({msg:resultOfAction})
+    } catch (error) {
+        res.status(500).json({error:error})
+    }
+}
+
 export const UserController = {
-    GetAll,
-    Create,
+    getById,
+    getAll,
+    create,
+    updateById,
+    deleteById
 }
--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -3,7 +3,10 @@ import { UserController } from '../controllers';
 
 const routes = Router();
 
-routes.get('/users', UserController.GetAll);
-routes.post('/users', UserController.Create);
+routes.get('/users', UserController.getAll);
+routes.get('/users/:id', UserController.getById);
+routes.post('/users', UserController.create);
+routes.put('/users/:id', UserController.updateById);
+routes.delete('/users/:id', UserController.deleteById);
 
 export { routes };                            
