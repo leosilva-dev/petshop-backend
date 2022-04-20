185b3813a609ce05ad6db7402f3add5533ba5d1e
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-04-20 14:38:12 -0300

    add auth middleware

---
+++ src/Middlewares/auth.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { celebrate, Joi } from 'celebrate';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+interface TokenPayload {
+    id: string;
+}
+
+const registerValidation = celebrate({
+    body: Joi.object({
+        name: Joi.string().required(),
+        username: Joi.string().required(),
+        email: Joi.string().required(),
+        bio: Joi.string().required(),
+        password: Joi.string().required(),
+    })
+}, {abortEarly: false});
+
+export const authMiddleware = (req: Request, res: Response, next:NextFunction) => {
+    const { authorization } = req.headers;
+
+    if(!authorization){
+        return res.status(StatusCodes.UNAUTHORIZED).json({msg: 'Unauthorized'})
+    }
+
+    const token = authorization.replace('Bearer', '').trim();
+
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET as string);
+        const { id } = data as TokenPayload;
+        
+        req.userId = id;
+
+        return next();
+    } catch(error) {
+        return res.status(401).json({ message: "Unauthorized"});
+    }
+}
+
+export const auth = {
+    registerValidation,
+}
