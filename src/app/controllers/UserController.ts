3f1f73d9701776e11108fd870cbd31dac76d02bf
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-04-21 01:55:15 -0300

    User controller methods updated with Links logic

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -2,12 +2,15 @@ import { StatusCodes } from 'http-status-codes';
 import {Request, Response} from 'express'
 
 import {User} from '../models/User'
+import { Link } from '../models/Links';
 
 const getById = async (req: Request, res: Response) => {
     try {
         const id = req.params.id
         const userById = await User.findById(id)
-        res.status(StatusCodes.OK).json(userById)        
+        const links = await Link.find({user: id})
+
+        res.status(StatusCodes.OK).json({userById, links})        
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({error:error})
     }
@@ -15,19 +18,42 @@ const getById = async (req: Request, res: Response) => {
 
 const getAll = async (_:Request, res: Response) => {
     try {
-        const allUsers = await User.find().select("-password")
+        const allUsers = await User.find().select("-password")        
         res.status(StatusCodes.OK).json(allUsers)
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({error:error})
     }
 }
 
+
+
 const updateById = async (req:Request, res:Response) => {
     try {
         const id = req.params.id
-        const {name, email, password, username, bio} = req.body
-        const userUpdated = await User.findByIdAndUpdate(id, {name, email, password, username, bio})
-        res.status(StatusCodes.OK).json({userUpdated})
+
+        const {name, email, username, bio, links} = req.body
+
+        await User.findByIdAndUpdate(id, {name, email, username, bio})
+
+        await Link.deleteMany({user: id})
+
+        await Promise.all(
+            links.map(async (link:any) => {
+                const newLink = new Link({
+                    title: link.title,
+                    url: link.url,
+                    enabled: link.enabled,
+                    order: link.order,
+                    user: id
+                })
+                return await newLink.save()
+            })
+        )
+
+        const userUpdated = await User.findById(id)
+        const linksUpdateds = await Link.find({user: id})
+
+        res.status(StatusCodes.OK).json({userUpdated,linksUpdateds})
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({error:error})
     }
@@ -37,6 +63,8 @@ const deleteById = async (req: Request, res: Response) => {
     try {
         const id = req.params.id
         const resultOfAction = await User.deleteOne({_id: id})
+        await Link.deleteMany({user: id})
+
         res.status(StatusCodes.OK).json({msg:resultOfAction})
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({error:error})
