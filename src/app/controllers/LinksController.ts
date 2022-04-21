3f357a37601b086b6b6de6357a2eeb318a39c6ee
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-04-21 01:54:48 -0300

    Create Links controller

--- src/app/controllers/LinksController.ts
+++ src/app/controllers/LinksController.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express"
 import { StatusCodes } from "http-status-codes"
+import { Link } from "../models/Links"
 
+const getAll = async (req: Request, res: Response) => {
+    try{
+        const allLinks = await Link.find({user: req.userId})
+        allLinks.sort((a, b) => a.order - b.order)
 
+        res.status(StatusCodes.OK).json({links:allLinks})
 
-
-
-
+    }catch(err){
+        console.log(err)
+        return res.status(StatusCodes.BAD_REQUEST).json({error:err})
+    }
+}
 
 export const LinksController = {
-    
+    getAll
 }
