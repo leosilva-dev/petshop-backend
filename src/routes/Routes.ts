6107bdcd9f71d5a2d098a505ef4a65a54b6bfa6b
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-04-21 01:55:51 -0300

    Add authMiddleware to user controller routes and add get links route

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -5,16 +5,15 @@ import {auth, authMiddleware} from '../app/Middlewares';
 
 const routes = Router();
 
-routes.get('/users', UserController.getAll);
-routes.get('/users/:id', UserController.getById);
-routes.put('/users/:id', UserController.updateById);
-routes.delete('/users/:id', UserController.deleteById);
+routes.get('/users', authMiddleware, UserController.getAll);
+routes.get('/users/:id', authMiddleware, UserController.getById);
+routes.put('/users/:id', authMiddleware, UserController.updateById);
+routes.delete('/users/:id', authMiddleware, UserController.deleteById);
 
 routes.post('/login', AuthController.login);
 routes.post('/register', auth.registerValidation, AuthController.register);
 
-routes.get('/hello', authMiddleware, (req, res) => {
-    return res.json({ msg: 'Hello!!!' });
-});
+routes.get('/links', authMiddleware, LinksController.getAll);
+
 
 export { routes };                            
