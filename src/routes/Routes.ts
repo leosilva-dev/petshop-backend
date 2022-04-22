437d805d6a9ec454554e8d37f50aa8b8e30a5d8a
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-04-22 02:33:01 -0300

    Adding new routes do API

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -5,8 +5,10 @@ import {auth, authMiddleware} from '../app/Middlewares';
 
 const routes = Router();
 
+routes.get('/user', authMiddleware, UserController.getData);
 routes.get('/users', authMiddleware, UserController.getAll);
 routes.get('/users/:id', authMiddleware, UserController.getById);
+routes.put('/users/', authMiddleware, UserController.updateById);
 routes.put('/users/:id', authMiddleware, UserController.updateById);
 routes.delete('/users/:id', authMiddleware, UserController.deleteById);
 
@@ -15,5 +17,6 @@ routes.post('/register', auth.registerValidation, AuthController.register);
 
 routes.get('/links', authMiddleware, LinksController.getAll);
 
+routes.get('/@/:username', UserController.getProfilePublicData);
 
 export { routes };                            
