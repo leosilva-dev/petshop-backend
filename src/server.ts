e78e3e891f20b3c58e20c9fa27a4635efd152968
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-04-22 02:30:41 -0300

    add cors config

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ require('dotenv/config');
 
 import express from 'express';
 import { errors } from "celebrate";
+import cors from "cors";
+
 import { routes } from "./routes/Routes";
 import {connectToDatabase} from './database/Database'
 
 connectToDatabase();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
