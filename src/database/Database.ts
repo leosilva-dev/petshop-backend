8e37e4c9e37a5264c356ed010c5ad2995fa81b05
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-11-04 20:24:09 -0300

    criando conexão com banco de dados

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,8 +1,11 @@
 require('dotenv/config');
 import  mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 function connectToDatabase(){
-mongoose.connect('mongodb://localhost:27017/mylinks', {autoIndex:true, autoCreate:true});
+mongoose.connect(process.env.MONGODB_URL_CONNECTION as string, {autoIndex:true, autoCreate:true});
   const db = mongoose.connection;
 
   db.on("error", (error) => console.error('error '+error));
